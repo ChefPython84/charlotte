81981f4d4f43bb330585660b7be051d787adbd9f
ChefPython84 <erwan.couturier84@outlook.fr>
2025-10-30 18:20:12 +0100

    correction du bug

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    Symfony\Bundle\WorkflowBundle\WorkflowBundle::class => ['all' => true],
 ];
